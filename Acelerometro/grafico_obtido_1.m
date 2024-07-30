7a9b314f729eda4a4783fdceebe940edb59b204b
LuanPacheco11 <luan.vitor.pacheco@uel.br>
2024-07-30 11:14:36 -0300

    Graficos atualizados

--- Acelerometro/grafico_obtido_1.m
+++ Acelerometro/grafico_obtido_1.m
@@ -1,10 +1,10 @@
-<<<<<<< HEAD
 clear;
 clc;
 close all;
 
 M = csvread("dados_acelerometro_1.csv");
 
+% Adquire os dados medidos do acelerometro
 theta = M(33:45,1);
 c21 = M(33:45,2);
 c32 = M(33:45,3);
@@ -12,49 +12,44 @@ c21d = c21-c21(1);
 c32d = c32-c32(1);
 
 
+% deixa os dados medidos na mesma proporção dos modelados
+theta = linspace(0, 0.103, 13); 
+
 Eq = csvread("acelerometro_eq_pontos.csv");
 
-C12_eq = Eq(1:114,1)';
-C32_eq = Eq(115:228,1)';
+% Adquire os dados modelados do acelerometro
+C12_eq = Eq(1:103,1)'*10;
+C32_eq = Eq(115:217,1)'*10;
 
 C21d_eq = C12_eq-C12_eq(1);
 C32d_eq = C32_eq-C32_eq(1);
 
-theta_eq = linspace(0, 1.13, 114);
+theta_eq = linspace(0, 0.103, 103);
 
 
+% Plot dos dois dados comparando a capacitancia com o angulo
 figure(1)
+title("Capacitancia pelo angulo(°)")
 subplot(2,1,1)
 plot(theta_eq, C21d_eq, 'r', theta_eq, C32d_eq, 'b')
-
-
+subtitle("Modelado")
+xlabel("Angulo (°)")
+ylabel("Capacitancia (pF)")
 subplot(2,1,2)
 plot(theta, c21d, "r", theta, c32d, "b")
-legend("C12", "C32")
+subtitle("Obtidos")
+xlabel("Angulo (°)")
+ylabel("Capacitancia (pF)")
 
+% Plot comparando a variacao entre eles
 figure(2)
+title("Variação C1 por C2")
 subplot(2,1,1)
 plot(C21d_eq, C32d_eq, "r")
+title("Modelada")
+xlabel("C2")
+ylabel("C1")
+
 subplot(2,1,2)
-=======
-clear;
-clc;
-close all;
-
-M = csvread("dados_acelerometro_1.csv");
-
-theta = M(33:45,1);
-c21 = M(33:45,2);
-c32 = M(33:45,3);
-c21d = c21-c21(1);
-c32d = c32-c32(1);
-
-
-
-figure(1)
-plot(theta, c21d, "r", theta, c32d, "b")
-legend("C12", "C32")
-
-figure(2)
->>>>>>> 0015cab0cb0a83d25770e29c7fdb18a61f97b6d9
 plot(c21d, c32d, "r")
+title("Obtido")
